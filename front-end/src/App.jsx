e0bcf2a133acd48416ffd4a4400b8d89402f520d
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2023-10-07 18:14:13 -0300

    Add Loader components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import { allRoutes } from './routes/routes'
+import { Loader } from './components/loader'
 const CrudProducts = lazy(() => import('./pages/crud-products/CrudProducts'))
 const HomeAdmin = lazy(() => import('./pages/home-admin/HomeAdmin'))
 
 function App () {
   return (
-    <Suspense fallback={<div>Loader...</div>}>
+    <Suspense fallback={<Loader />}>
       <BrowserRouter>
         <Routes>
           <Route path={allRoutes.CRUD_PRODUCTS} element={<CrudProducts />} />
